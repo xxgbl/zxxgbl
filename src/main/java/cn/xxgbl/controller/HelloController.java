d3f403973f1ab61869de11d9c33f428c65d6ce0a
xxgbl <xxgbl@nyist.edu.cn>
2021-12-14 23:03:54 +0800

    第三次上传

--- src/main/java/cn/xxgbl/controller/HelloController.java
+++ src/main/java/cn/xxgbl/controller/HelloController.java
@@ -11,4 +11,10 @@ public class HelloController {
     public String hello(){
         return "hello";
     }
+
+    @RequestMapping("/login")
+    @ResponseBody
+    public String login(){
+        return "login";
+    }
 }
